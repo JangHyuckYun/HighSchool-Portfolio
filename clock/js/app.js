dc7fa76429710dd92e7a7f0cd6b26393c88b3b01
by09019 <skg09203@naver.com>
2020-01-09 16:17:59 +0900

    test

---
+++ clock/js/app.js
@@ -0,0 +1,82 @@
+// setting
+const
+center = "translate(-50%, -50%)",
+one = v => document.querySelector(v),
+all = v => Array.from(document.querySelectorAll(v)),
+start = _ => startInterval(1000, work),
+stop = _ => clearInterval(clock);
+
+let 
+clock, 
+nbChk = "",
+hour = -1, 
+minute = -1, 
+second = -1;
+
+numberMaxLength = e => { // input 글자수 제한
+	if(e.value.length > e.maxLength){
+		e.value = e.value.slice(0, e.maxLength);
+	}
+}
+
+modify = _ => { // 시간 수정
+	let text =  one(".modify").innerHTML;
+	if(text === "수정"){
+		clearInterval(clock);
+		one(".modify").innerHTML = "수정완료";
+	}else if(text === "수정완료") {
+		hour = one("input[name='hour']").value;
+		minute = one("input[name='minute']").value;
+		second = one("input[name='second']").value;
+
+		if(hour == "" || minute == "" || second == "") return alert("값을 넣어 주세요.");
+		else if(hour > 12 || hour < 1 || minute > 60 || minute < 0 || second > 60 || second < 0) return alert("정상적인 값을 넣어주세요.");
+		neddle_position((hour * 30), (minute * 6), (second * 6));
+		clock = setInterval(work, 1000);
+
+		one(".nb").innerHTML = one(".nbCheck").value;
+		one(".modify").innerHTML = "수정";
+		all(".val").forEach(v => {
+			v.value = "";
+		});
+	}
+	
+	one(".modifyPage").classList.toggle("hidden");
+}
+
+neddle_position = (hour, minute, second) => { // 시곗바늘 위치 조정
+	one(".hour").style.transform = center+` rotate(${hour}deg)`;
+	one(".minute").style.transform = center+` rotate(${minute}deg)`;
+	one(".second").style.transform = center+` rotate(${second}deg)`;
+}
+
+startInterval = (seconds, callback) => { // setInterval callback
+	callback();
+	return clock = setInterval(callback, seconds);
+}
+
+work = _ => { // 메인 함수
+	if(hour == -1, minute == -1, second == -1){
+		let date = new Date();
+		nbChk = date.getHours() < 12 ? "a.m" : "p.m";
+		one(".nb").innerHTML = nbChk;
+		neddle_position((date.getHours() * 30), (date.getMinutes() * 6), (date.getSeconds() * 6));
+	}else {
+		if(second > 59){
+			second = 0;
+			++minute;
+		}
+
+		if(minute > 59) {
+			minute = 0;
+			++hour;
+		}
+
+		if(hour > 12) hour = 1;
+
+		++second;
+		neddle_position((hour * 30), (minute * 6), (second * 6));
+	}
+}
+
+startInterval(1000, work);
